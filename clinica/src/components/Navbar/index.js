b94c13ef78479cfde2ade8f54dea2df2adc6c84c
Carlos Eduardo <cadu.ce3@gmail.com>
2023-07-07 16:39:19 -0300

    estilizando o toggle

--- clinica/src/components/Navbar/index.js
+++ clinica/src/components/Navbar/index.js
@@ -25,7 +25,7 @@ export default function index() {
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-verde-prototipo hover:bg-verde-prototipo hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-verde-prototipo">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -69,7 +69,7 @@ export default function index() {
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-gray-900 text-white' : 'text-verde-prototipo hover:bg-verde-prototipo hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
